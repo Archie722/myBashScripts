efe25184f3e5c55222df88fc76a4260ced747958
archie722 <m_james722@hotmail.com>
2022-08-29 15:47:45 -0400

    ping-sweep.sh v1

---
+++ ping-sweep/ping-sweep.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+echo 'Enter the /24 network address you would like to ping-sweep'
+read userinput
+# Get the first three octects of the IP address the user inputs
+networkaddress=$(echo $userinput | cut -d '.' -f 1,2,3)
+# Ping all IP addresses in the network and show the ones that reply
+for i in {1..254}; do (ping -c 1 $networkaddress.$i | grep "bytes from" | cut -d ' ' -f 4 | cut -d ':' -f 1 &); done
